346b03534407ae7b0eb7f3a2ccbf591d30c61f48
Tripper678 <samallenyepthatsme@gmail.com>
2023-06-05 20:15:39 -0400

    Styling changes made.

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
 import { WeatherProvider } from './context/weatherContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
